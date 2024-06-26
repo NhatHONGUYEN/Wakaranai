2bc3c0fd3c8cd4d195d0d7d5ce2e1546da2ba062
NhatHONGUYEN <hnnhat67@gmail.com>
2024-06-26 20:05:28 +0200

    ✨ F01 : Add Clothes for the data

---
+++ src/utils/Clothes.jsx
@@ -0,0 +1,100 @@
+export const Clothes = [
+  {
+    Clothes: {
+      female: {
+        items: [
+          {
+            name: "Oversize Shirt",
+            price: 39.99,
+            sizes: ["S", "M", "L", "XL"],
+            quantity: 20,
+            image: [
+              "../assets/images/chemiseoversivefemme (1).jpg",
+              "../assets/images/chemiseoversivefemme (2).jpg",
+              "../assets/images/chemiseoversivefemme (3).jpg",
+            ],
+            videos: "../assets/images/womanVideo (3).mp4",
+          },
+          {
+            name: "Checkered Vest",
+            price: 59.99,
+            sizes: ["M", "L"],
+            quantity: 15,
+            image: [
+              "../assets/images/vesteaCarreau (1).jpg",
+              "../assets/images/vesteaCarreau (2).jpg",
+              "../assets/images/vesteaCarreau (3).jpg",
+              "../assets/images/vesteaCarreau (4).jpg",
+            ],
+          },
+          {
+            name: "Suit",
+            price: 129.99,
+            sizes: ["S", "M", "L"],
+            quantity: 10,
+            image: [
+              "../assets/images/costumeEnLinFemme (1).jpg",
+              "../assets/images/costumeEnLinFemme (2).jpg",
+              "../assets/images/costumeEnLinFemme (3).jpg",
+              "../assets/images/costumeEnLinFemme (4).jpg",
+            ],
+            videos: "../assets/images/womanVideo (2).mp4",
+          },
+          {
+            name: "Trench Coat",
+            price: 89.99,
+            sizes: ["S", "M", "L", "XL"],
+            quantity: 12,
+            images: [
+              "../assets/images/trench (1).jpg",
+              "../assets/images/trench (2).jpg",
+              "../assets/images/trench (3).jpg",
+              "../assets/images/trench (4).jpg",
+            ],
+            videos: "../assets/images/womanVideo (1).mp4",
+          },
+        ],
+      },
+      male: {
+        items: [
+          {
+            name: "Oversize Shirt",
+            price: 49.99,
+            sizes: ["S", "M", "L", "XL"],
+            quantity: 18,
+            image: [
+              ".../assets/images/chemiseOversizeHomme (1).jpg",
+              ".../assets/images/chemiseOversizeHomme (2).jpg",
+              ".../assets/images/chemiseOversizeHomme (3).jpg",
+            ],
+            videos: [
+              "../assets/images/ManVideo (1).mp4",
+              "../assets/images/ManVideo (2).mp4",
+            ],
+          },
+          {
+            name: "Black Shirt",
+            price: 29.99,
+            sizes: ["S", "M", "L", "XL"],
+            quantity: 25,
+            image: [
+              "../assets/images/chemiseNoir (1).jpg",
+              "../assets/images/chemiseNoir (2).jpg",
+            ],
+          },
+          {
+            name: "Pullover",
+            price: 39.99,
+            sizes: ["S", "M", "L", "XL"],
+            quantity: 16,
+            image: [
+              "../assets/images/pullHomme (1).jpg",
+              "../assets/images/pullHomme (2).jpg",
+              "../assets/images/pullHomme (3).jpg",
+            ],
+          },
+        ],
+      },
+    },
+  },
+];
