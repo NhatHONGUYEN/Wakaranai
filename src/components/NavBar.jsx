fb1b956d2973955405a6d747b4f307efc3b8464d
NhatHONGUYEN <hnnhat67@gmail.com>
2024-07-03 09:48:44 +0200

    💄 F01 : change the navbarleft was too small for mobile

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,8 @@
-import { Link, useNavigate } from "react-router-dom";
 import NavBarRight from "./NavBarRight";
 import { motion } from "framer-motion";
+import NavBarLeft from "./NavBarLeft";
 
 export default function NavBar() {
-  const navigate = useNavigate();
   return (
     <motion.div
       initial={{ opacity: 0, y: -50 }}
@@ -15,56 +14,7 @@ export default function NavBar() {
       }}
       className="navbar absolute top-0 z-40 "
     >
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h7"
-              />
-            </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm bg-indigo-600 dropdown-content text-xl rounded-box z-[1] mt-3 w-32 py-4  pr-8 shadow text-slate-200 transform scale-150 transition-transform duration-2000"
-            style={{ transformOrigin: "top left" }}
-          >
-            <li>
-              <button
-                onClick={() => navigate("/")}
-                className="hover:translate-x-2 transition-transform hover:slate-200"
-              >
-                Homepage
-              </button>
-            </li>
-            <li>
-              <Link
-                to="/womanshop"
-                className="hover:translate-x-2 transition-transform hover:slate-200"
-              >
-                Women
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/manshop"
-                className="hover:translate-x-2 transition-transform hover:slate-200"
-              >
-                Men
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <NavBarLeft />
       <div className="navbar-center hidden lg:block">
         <p className=" text-xl ">WAKARANAI</p>
       </div>
