69b548359a49d4bf6c1df5964ae5a0870179b39a
NhatHONGUYEN <hnnhat67@gmail.com>
2024-07-03 18:48:58 +0200

    ✨   F02 : LOGIN> Add padding for our input

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -54,7 +54,7 @@ export default function Login() {
                     >
                       Email address
                     </label>
-                    <div className="mt-2">
+                    <div className="mt-2 ">
                       <input
                         onChange={(e) => setEmail(e.target.value)}
                         id="email"
@@ -62,7 +62,7 @@ export default function Login() {
                         type="email"
                         autoComplete="email"
                         required
-                        className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        className="block w-full pl-2 rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                       />
                     </div>
                   </div>
@@ -70,7 +70,7 @@ export default function Login() {
                   <div>
                     <label
                       htmlFor="password"
-                      className="block text-sm font-medium leading-6 text-gray-900"
+                      className="block  text-sm font-medium leading-6 text-gray-900"
                     >
                       Password
                     </label>
@@ -82,7 +82,7 @@ export default function Login() {
                         type="password"
                         autoComplete="current-password"
                         required
-                        className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        className="block  w-full pl-2 rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                       />
                     </div>
                   </div>
